03c45ab8b8b7342371a9086efd28d677f1fbb0b2
Siri Mudumbi <siri.mudumbi@sjsu.edu>
2024-04-09 15:09:01 -0700

    added database

--- mawls/api/index.py
+++ mawls/api/index.py
@@ -1,6 +1,35 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_cqlalchemy import CQLAlchemy
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app = Flask(__name__)
+app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+app.config['CASSANDRA_KEYSPACE'] = 'mawls'
+
+db = CQLAlchemy(app)
+
+class User(db.Model):
+    user_id = db.columns.UUID(primary_key=True)
+    username = db.columns.Text(required=True)
+    password = db.columns.Text(required=True)
+    email = db.columns.Text(required=True)
+
+class Lounge(db.Model):
+    lounge_id = db.columns.UUID(primary_key=True)
+    lounge_name = db.columns.Text(required=True)
+    lounge_members = db.columns.List(db.columns.Text)
+
+class Message(db.Model):
+    message_id = db.columns.UUID(primary_key=True)
+    channel_id = db.collumns.UUID(required=True)
+    sender_name = db.columns.Text(required=True)
+    content = db.columns.Text(required=True)
+    timestamp = db.columns.DateTime(required=True)
+
+class Channel(db.Model):
+    channel_id = db.columns.UUID(primary_key=True)
+    channel_name = db.columns.Text(required=True)
+    lounge_id = db.columns.UUID(required=True)
+
+# Create tables
+db.create_all()
+
